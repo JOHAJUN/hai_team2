030645ed85d4892b7c835b2914b44f3af315ec2b
202110836 <nagabuti13@naver.com>
2025-05-16 08:36:45 +0900

    AudioExtractor.py, Transcription.py 추가 및 YoutubeDownloader.py와 main.py 일부 수정

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from Module.YoutubeDownloader import download_youtube_video
+from Module.Transcription import *
 
 def main():
     url = input("Enter YouTube video URL: ").strip()
 
-    output_path = download_youtube_video(url)
+    merged_path, _, audio_path = download_youtube_video(url)
 
-    if output_path:
-        print(f"\nDownload complete! Saved to: {output_path}")
+    if merged_path:
+        print(f"\nDownload complete! Saved to: {merged_path}")
     else:
         print("\nDownload or merge failed.")
-
+        
+    print('\nTranscribing audio...')    
+    transcription, audio = audio_transcribe(audio_path)
+    save_transcript_json(transcription, audio)
+    save_transcript_srt(transcription, audio)
+    
+    if transcription:
+        print("\nTranscription complete!")
+    else:
+        print("\nTranscription failed.")
+    
 if __name__ == "__main__":
     main()
