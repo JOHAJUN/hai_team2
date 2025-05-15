7fc62bd6992f66b1958586313144220156ef3064
202110836 <nagabuti13@naver.com>
2025-05-16 08:56:10 +0900

    YoutubeDownloader.py에 코드 추가

--- Module/YoutubeDownloader.py
+++ Module/YoutubeDownloader.py
@@ -1,6 +1,7 @@
 import os
 from pytubefix import YouTube
 import ffmpeg
+from Module.AudioExtractor import convert_m4a_to_wav
 
 def get_video_size(stream):
     return stream.filesize / (1024 * 1024)  # MB 단위로 변환
@@ -33,25 +34,31 @@ def download_youtube_video(url, save_dir='Input'):
         # Adaptive 스트림이면 오디오 따로 다운로드 후 병합
         if not selected_stream.is_progressive:
             print("Downloading audio stream...")
-            audio_file = audio_stream.download(output_path=save_dir, filename_prefix="audio_")
-
+            
+            # 오디오 파일을 다시 인코딩하여 저장(코덱 정보가 손상된 기존 오디오 파일 복구)
+            audio_file = audio_stream.download()
+            new_audio_file = os.path.join(save_dir, f"audio_{yt.title}.m4a")
+            ffmpeg.input(audio_file).output(new_audio_file, acodec='aac', strict='experimental').run(overwrite_output=True)
+            os.remove(audio_file)
+            
+            # 비디오 및 오디오 병합
             print("Merging video and audio...")
-            output_file = os.path.join(save_dir, f"{yt.title}_merged.mp4")
+            merged_file = os.path.join(save_dir, f"{yt.title}_merged.mp4")
 
             video_input = ffmpeg.input(video_file)
-            audio_input = ffmpeg.input(audio_file)
-            ffmpeg.output(video_input, audio_input, output_file,
+            audio_input = ffmpeg.input(new_audio_file)
+            ffmpeg.output(video_input, audio_input, merged_file,
                           vcodec='libx264', acodec='aac', strict='experimental').run(overwrite_output=True)
 
-            # 병합 후 임시 파일 삭제
-            os.remove(video_file)
-            os.remove(audio_file)
+            # 오디오 파일을 WAV로 변환
+            convert_m4a_to_wav(new_audio_file)
+
         else:
             # Progressive면 바로 완료
-            output_file = video_file
+            merged_file = video_file
 
-        print(f"Download and merge completed: {output_file}")
-        return output_file
+        print(f"Download and merge completed: {merged_file}")
+        return merged_file, video_file, new_audio_file
 
     except Exception as e:
         # 오류 발생 시 출력
